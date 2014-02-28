cf27dd855d2b9c6e0b5828fdb7bda0e79ec72f4b
aentan <hello@aentan.com>
2014-02-28 11:01:22 +0800

    middleman site

---
+++ Gemfile
@@ -0,0 +1,27 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
+source 'https://rubygems.org'
+
+gem "middleman", "~> 3.2.2"
+
+# Other gems
+gem 'therubyracer'
+gem 'oj'
+
+# Minify HTML
+gem "middleman-minify-html"
+
+# Use Sass
+gem 'sass'
+
+# Livereload
+gem "middleman-livereload", "~> 3.1.0"
+
+# Imageoptim
+gem "middleman-imageoptim", "~> 0.1.3"
+
+# Used for publishing to Github Pages
+gem 'middleman-deploy'
+
+# For HTML parsing
+gem "nokogiri"
---
+++ source/assets/javascripts/application.js
@@ -0,0 +1,19 @@
+//= require 'vendor/jquery.gsap'
+//= require 'vendor/jquery.fullPage'
+
+$(function() {
+  $.fn.fullpage({
+    verticalCentered: false,
+    resize : false,
+    scrollingSpeed: 800,
+    easing: 'easeOutQuad',
+    menu: false,
+    navigation: true,
+    navigationTooltips: ['Home', 'Live Contacts', 'Search', 'Privacy', 'Duplicates', 'Backup', 'Contact'],
+    slidesNavigation: true,
+    slidesNavPosition: 'right',
+    loopHorizontal: true,
+    autoScrolling: true,
+    keyboardScrolling: true
+  });
+});
